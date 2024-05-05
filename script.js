fb29b3f67ecea0b0154ccf5705b3cdbd4b5367b6
Syed Rayyan Ali <149191182+RayyanWebDevelopment@users.noreply.github.com>
2024-05-05 04:16:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Calculator Program//
+let display = document.getElementById('display');
+
+
+function appendtodisplay(input){
+    display.value += input;
+
+}
+function cleardisplay(){
+    display.value ="";
+}
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Sahi Number Dal :)";
+    }
+}
+
